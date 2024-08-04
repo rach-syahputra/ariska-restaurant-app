9a91194e5c575dd01bcda75ae70bb50694b01946
rach syahputra <rach.syahputra@gmail.com>
2024-08-05 07:38:33 +0800

    add getRestaurantDetail endpoint

--- src/globals/api-endpoint.js
+++ src/globals/api-endpoint.js
@@ -4,7 +4,10 @@ const API_ENDPOINT = {
   getRestaurantImage: (pictureId) => {
     return `${CONFIG.BASE_URL}/images/large/${pictureId}`
   },
-  getRestaurantList: `${CONFIG.BASE_URL}/list`
+  getRestaurantList: `${CONFIG.BASE_URL}/list`,
+  getRestaurantDetail: (id) => {
+    return `${CONFIG.BASE_URL}/detail/${id}`
+  }
 }
 
 export default API_ENDPOINT

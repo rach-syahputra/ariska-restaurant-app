d3d2a2c4ae37862cce64be29b8d852846689dc26
rach syahputra <rach.syahputra@gmail.com>
2024-08-06 01:52:55 +0800

    add favorites page

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
+import Favorites from '../scripts/views/pages/favorites'
 import Home from '../scripts/views/pages/home'
 import RestaurantDetail from '../scripts/views/pages/restaurant-detail'
 
 const routes = {
   '/': Home,
-  '/detail/:id': RestaurantDetail
+  '/detail/:id': RestaurantDetail,
+  '/favorites': Favorites
 }
 
 export default routes
---
+++ src/scripts/views/pages/favorites.js
@@ -0,0 +1,33 @@
+import FavoriteRestaurantIdb from '../../../data/favorite-restaurant-idb'
+
+const Favorites = {
+  async render() {
+    return `
+      <favorite-list></favorite-list>
+    `
+  },
+
+  async afterRender() {
+    const restaurants = await FavoriteRestaurantIdb.getAllRestaurants()
+
+    this.populateRestaurantDataToRestaurantItem(restaurants)
+  },
+
+  populateRestaurantDataToRestaurantItem(restaurants) {
+    const favoriteList = document.querySelector('favorite-list')
+
+    restaurants.forEach((restaurant) => {
+      const restaurantItem = this.createRestaurantItem(restaurant)
+      favoriteList.appendChild(restaurantItem)
+    })
+  },
+
+  createRestaurantItem(restaurant) {
+    const restaurantItem = document.createElement('restaurant-item')
+    restaurantItem.restaurant = restaurant
+
+    return restaurantItem
+  }
+}
+
+export default Favorites

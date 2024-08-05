a4d76e370b4eb6874f36b90d5aa714863661576e
rach syahputra <rach.syahputra@gmail.com>
2024-08-05 08:50:04 +0800

    add restaurant-food-menu custom element

---
+++ src/scripts/components/restaurant-food-menu.js
@@ -0,0 +1,78 @@
+class RestaurantFoodMenu extends HTMLElement {
+  constructor() {
+    super()
+
+    this._shadowRoot = this.attachShadow({ mode: 'open' })
+    this._style = document.createElement('style')
+
+    this.render()
+  }
+
+  _emptyContent() {
+    this._shadowRoot.innerHTML = ''
+  }
+
+  _updateStyle() {
+    this._style.textContent = ` 
+      * {
+        box-sizing: border-box;
+        margin: 0;
+      }
+
+      .restaurant-food {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 32px;
+        margin-top: 32px;
+        padding: 16px;
+      }
+      
+      .restaurant-food__label {
+        font-weight: 100;
+        font-size: 24px;
+        width: 250px;
+        text-align: center;
+      }
+
+      .restaurant-food__label::after {
+        content: '';
+        margin-top: 16px;
+        display: block;
+        border-bottom: 1px solid #d3d3d3;
+      }
+
+      .restaurant-food__list {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        padding-inline-start: 0;
+        gap: 16px;
+
+        li {
+          list-style-type: none;
+          font-size: 18px;
+        }
+      }
+    `
+  }
+
+  render() {
+    this._emptyContent()
+    this._updateStyle()
+
+    this._shadowRoot.appendChild(this._style)
+
+    this._shadowRoot.innerHTML += `
+    <article class="restaurant-food">
+      <h1 class="restaurant-food__label">FOOD</h1>
+      <ul class="restaurant-food__list" id="foodList">
+      </ul>
+    </article>
+    `
+  }
+}
+
+customElements.define('restaurant-food-menu', RestaurantFoodMenu)

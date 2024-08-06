e8062a1bd8e95f93ea0f82131613541f01d04f25
rach syahputra <rach.syahputra@gmail.com>
2024-08-07 00:30:58 +0800

    migrate hero-element to shadow dom

--- src/scripts/components/hero-element.js
+++ src/scripts/components/hero-element.js
@@ -2,17 +2,50 @@ class HeroElement extends HTMLElement {
   constructor() {
     super()
 
+    this._shadowRoot = this.attachShadow({ mode: 'open' })
+    this._style = document.createElement('style')
+
     this.render()
   }
 
   _emptyContent() {
-    this.innerHTML = ''
+    this._shadowRoot.innerHTML = ''
+  }
+
+  _updateStyle() {
+    this._style.textContent = `  
+      * {
+        box-sizing: border-box;
+        margin: 0;
+      }    
+
+      .hero {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin: auto;
+        width: 100%;
+        min-height: 420px;
+        text-align: center;
+        background-image: url('images/heros/hero-image_2.jpg');
+        background-position: center;
+      }
+
+      @media screen and (min-width: 1200px) {
+        .hero {
+          max-width: 1200px;
+        }
+      }
+    `
   }
 
   render() {
     this._emptyContent()
+    this._updateStyle()
 
-    this.innerHTML += `
+    this._shadowRoot.appendChild(this._style)
+
+    this._shadowRoot.innerHTML += `
       <div class="hero"></div>
     `
   }

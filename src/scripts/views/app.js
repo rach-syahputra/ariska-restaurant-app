e92ba4e3b6da704bf495a749a4abb8a7e1cc3421
rach syahputra <rach.syahputra@gmail.com>
2024-08-07 03:42:52 +0800

    add button and drawer constructor for App and integrate DrawerInitiator

--- src/scripts/views/app.js
+++ src/scripts/views/app.js
@@ -1,9 +1,22 @@
 import routes from '../../routes/routes'
 import UrlParser from '../../routes/url-parser'
+import DrawerInitiator from '../../utils/drawer-initiator'
 
 class App {
-  constructor({ content }) {
+  constructor({ button, drawer, content }) {
+    this._button = button
+    this._drawer = drawer
     this._content = content
+
+    this._initialAppShell()
+  }
+
+  _initialAppShell() {
+    DrawerInitiator.init({
+      button: this._button,
+      drawer: this._drawer,
+      content: this._content
+    })
   }
 
   async renderPage() {

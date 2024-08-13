953002ed80bb4341d1a0c38f7d96680a0e15f2da
rach syahputra <rach.syahputra@gmail.com>
2024-08-13 15:12:47 +0800

    add 'liking a restaurant' e2e test scenario

--- e2e/Liking_Restaurants.test.js
+++ e2e/Liking_Restaurants.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
-// const assert = require('assert')
+const assert = require('assert')
 
 Feature('Liking Restaurants')
 
@@ -9,4 +9,24 @@ Before(({ I }) => {
 
 Scenario('showing empty favorited restaurants', ({ I }) => {
   I.see('No favorite restaurants to display :(', '#noFavorites')
+})
+
+Scenario('liking a restaurant', async ({ I }) => {
+  I.see('No favorite restaurants to display :(', '#noFavorites')
+
+  I.amOnPage('/')
+
+  I.seeElement('.restaurant-item__name')
+  const firstRestaurant = locate('.restaurant-item__name').first()
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant)
+  I.click(firstRestaurant)
+
+  I.seeElement('favorite-button')
+  I.click('favorite-button')
+
+  I.amOnPage('/#/favorites')
+  I.seeElement('.restaurant-item')
+  const favoritedRestaurantName = await I.grabTextFrom('.restaurant-item__name')
+
+  assert.strictEqual(firstRestaurantName, favoritedRestaurantName)
 })

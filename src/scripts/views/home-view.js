e64f25a3dbde031afc46a5f7111d99115ff22e9a
rach syahputra <rach.syahputra@gmail.com>
2024-08-14 19:41:06 +0800

    improve CLS

--- src/scripts/views/home-view.js
+++ src/scripts/views/home-view.js
@@ -9,7 +9,7 @@ class HomeView {
         <article class="restaurant">
           <h1 class="restaurant__label">Explore Restaurants</h1>
           <slot name="loading"></slot>
-          <div class="restaurants" id="restaurantList">
+          <div class="restaurants" id="restaurantList">    
           </div>
         </article>
       </div>
@@ -26,9 +26,30 @@ class HomeView {
       html = restaurants.reduce((carry, restaurant) => carry.concat(createRestaurantItemTemplate(restaurant)), '')
     }
 
+    this._emptyRestaurantList()
+
     document.getElementById('restaurantList').innerHTML = html
 
     this.getContainer().dispatchEvent(new Event('restaurants:updated'))
+  }
+
+  showLoading() {
+    for (let i = 1; i < 20; i++) {
+      document.getElementById('restaurantList').innerHTML += `
+        <div class="skeleton-item">
+          <div class="skeleton-item__thumbnail"></div>
+          <div class="skeleton-item__content">
+            <div class="skeleton-item__rating"></div>
+            <div class="skeleton-item__name"></div>
+            <div class="skeleton-item__description"></div>
+          </div>
+        </div>
+      `
+    }
+  }
+
+  _emptyRestaurantList() {
+    document.getElementById('restaurantList').innerHTML = ''
   }
 }
 

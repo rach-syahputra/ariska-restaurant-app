53f3cba721325886beeadb47230ccfec8a5944f8
rach syahputra <rach.syahputra@gmail.com>
2024-08-15 17:33:07 +0800

    add hero tagline

--- src/scripts/components/hero-element.js
+++ src/scripts/components/hero-element.js
@@ -25,16 +25,68 @@ class HeroElement extends HTMLElement {
         align-items: center;
         margin: auto;
         width: 100%;
-        height: 420px;
+        height: fit-content;
+        max-height: 420px;
         text-align: center;
-        background-position: center;
+        position: relative;
+        overflow: hidden;
       }
 
       img {
-          width: 100%;
-          height: 100%;
-          object-fit: cover;
-          object-position: center;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        object-position: center;
+        transform: scale(1.1);
+      }
+
+      .hero__tagline {
+        position: absolute;
+        left: 0;
+        bottom: 8px;
+        display: flex;
+        flex-direction: column;
+        gap: 16px;
+        padding: 16px;
+        z-index: 3;
+        color: #fff;
+        font-weight: 500;
+      }
+
+      .hero__tagline__top {
+        font-size: 16px;
+        text-align: left;
+      }
+
+      .hero__tagline__bottom {
+        width: fit-content;
+        font-size: 16px;
+        padding: 8px 16px;
+        background-color: #d61920;
+      }
+      
+      .overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        z-index: 2;
+        background-color: rgba(0,0,0,0.4);
+      }
+
+      @media screen and (min-width: 768px) {
+        .hero__tagline {
+          padding: 32px;
+        }
+
+        .hero__tagline__top {
+          font-size: 24px;
+        }
+
+        .hero__tagline__bottom {
+          padding: 12px 24px;
+        }
       }
 
       @media screen and (min-width: 1200px) {
@@ -57,6 +109,15 @@ class HeroElement extends HTMLElement {
           <source media="(max-width: 768px)" srcset="./images/hero-image_2-small.jpg">
           <img src='./images/hero-image_2-large.jpg' alt="hero image">
         </picture>
+
+        <div class="overlay"></div>
+
+        <div class="hero__tagline">
+          <p class="hero__tagline__top">Authentic flavors and unforgettable experiences</p>
+          <div class="hero__tagline__bottom">
+            Good food, good mood
+          </div>
+        </div>
       </div>
     `
   }
